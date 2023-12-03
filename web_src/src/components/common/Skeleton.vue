2d0bbce8d96ee8f965634948020538c914279af3
xiayoumo <597774608@qq.com>
2023-12-03 21:53:59 +0800

    init 仓库4

---
+++ web_src/src/components/common/Skeleton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="skeleton-box">
+    <el-skeleton-item variant="image" class="skeleton-image"/>
+    <div class="skeleton-content">
+      <el-skeleton :rows="contentRow" animated />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skeleton",
+  props: {
+    contentRow: {
+      type: Number,
+      default: 15
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '~@/components/common/base.scss';
+
+.skeleton-image{
+  width: 240px;
+  height: 240px;
+}
+.skeleton-content{
+  padding-top: 14px;
+}
+.skeleton-box{
+  padding: 14px;
+}
+</style>
